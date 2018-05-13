f639d4116034b22bd5fabfee3ab01d765cda500d
Aaron Curtis <foobarbecue@gmail.com>
2018-05-13 14:35:10 -0700

    Experiments in styling hvhv example, add plan of main app

--- precursors/hvhv_holoviews.py
+++ precursors/hvhv_holoviews.py
@@ -13,16 +13,22 @@ from threading import Thread
 from tornado import gen
 from functools import partial
 from obspy.clients.seedlink.easyseedlink import create_client
+import pytz
 
 renderer = hv.renderer('bokeh')
-blank_data = pd.DataFrame({'counts': [0], 'timestamp':[pd.datetime.now()]})
-seis_stream = hv.streams.Buffer(blank_data, index=False)
+blank_data = pd.DataFrame({'counts': [None], 'timestamp':[pd.datetime.now(tz=pytz.utc)]})
+seis_stream = hv.streams.Buffer(blank_data, index=False, length=100000)
 
 # Create a curve element with all options except for data
-curve = partial(hv.Curve, kdims='timestamp', vdims='counts')
+curve = partial(hv.Curve, kdims='timestamp', vdims='counts', label='HV.WOOD.EHZ')
 
 # Define DynamicMaps and display plot
 seis_dmap = hv.DynamicMap(curve, streams=[seis_stream])
+now = pd.datetime.now()
+time_extent = pd.Timedelta('1 hour')
+seis_dmap = seis_dmap.options({'Curve': {'width': 1200}})
+seis_dmap.redim.range(timestamp=(now - time_extent, now + time_extent))
+
 doc = renderer.server_doc(seis_dmap)
 
 @gen.coroutine
@@ -32,16 +38,15 @@ def update(data):
 def handle_trace(trace):
     # Have obspy give the times in POSIX format and convert using pandas' to_datetime. Couldn't find a way to use obspy
     # UTCDateTime directly.
-    times = pd.to_datetime(trace.times('timestamp'), origin='unix', unit='s')
+    times = pd.to_datetime(trace.times('timestamp'), origin='unix', unit='s', utc=True)
 
     data = pd.DataFrame({'timestamp': times, 'counts': trace.data})
     print(trace)
-    time.sleep(1)
     doc.add_next_tick_callback(partial(update, data=data))
 
 def obspy_worker():
     client = create_client('rtserve.iris.washington.edu', on_data=handle_trace)
-    client.select_stream('HV', 'WOOD', 'EHZ')
+    client.select_stream('HV', 'WOOD', '???')
     client.run()
 
 obspy_thread = Thread(target=obspy_worker)
